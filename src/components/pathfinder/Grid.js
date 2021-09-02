be2589c0dd14207c836b28adef8cb8e26a498074
Krasimir Mladenov <kdmladenov@yahoo.com>
2021-09-02 12:19:48 +0300

    code refactoring

--- src/components/pathfinder/Grid.js
+++ src/components/pathfinder/Grid.js
@@ -13,86 +13,64 @@ const Grid = ({ size }) => {
   const [endRow, setEndRow] = useState(Math.floor(Math.random() * rowCount));
   const [endCol, setEndCol] = useState(Math.floor(Math.random() * colCount));
   const [mouseIsPressed, setMouseIsPressed] = useState(false);
-  const [relocateStart, setRelocateStart] = useState(false);
-  const [relocateEnd, setRelocateEnd] = useState(false);
+  const [toggle, setToggle] = useState('');
 
   const handleMouseDown = (row, col) => {
     if (startRow === row && startCol === col) {
-      setRelocateStart(true);
-      setGrid(gridWithRelocatedStartEnd(row, col));
+      setToggle('start');
+      setGrid(newGridWithToggledNode(row, col));
     } else if (endRow === row && endCol === col) {
-      setRelocateEnd(true);
-      setGrid(gridWithRelocatedStartEnd(row, col));
+      setToggle('end');
+      setGrid(newGridWithToggledNode(row, col));
     } else {
-      setGrid(gridWithToggleWall(row, col));
+      setToggle('wall');
+      setGrid(newGridWithToggledNode(row, col));
     }
 
     setMouseIsPressed(true);
   };
 
   const handleMouseHover = (row, col) => {
-    if (!mouseIsPressed || relocateStart || relocateEnd || grid[row][col].isWall) return; //goes to handleMouseClick
-    setGrid(gridWithToggleWall(row, col));
+    if (!mouseIsPressed || toggle === 'start' || toggle === 'end' || grid[row][col].isWall) return; //goes to handleMouseClick
+    setGrid(newGridWithToggledNode(row, col));
   };
 
   const handleMouseUp = (row, col) => {
-    if (relocateStart) {
+    if (toggle === 'start') {
       setStartRow(row);
       setStartCol(col);
     }
-    if (relocateEnd) {
+    if (toggle === 'end') {
       setEndRow(row);
       setEndCol(col);
     }
     setMouseIsPressed(false);
-    setGrid(gridWithRelocatedStartEnd(row, col));
-    setRelocateStart(false);
-    setRelocateEnd(false);
+    setGrid(newGridWithToggledNode(row, col));
+    setToggle('');
   };
 
   const handleMouseDoubleClick = (row, col) => {
     if ((startRow === row && startCol === col) || (endRow === row && endCol === col)) return;
-    setGrid(gridWithToggleWeight(row, col));
+    setToggle('weight');
+    setGrid(newGridWithToggledNode(row, col));
   };
 
-  const gridWithToggleWall = (row, col) => {
+  const newGridWithToggledNode = (row, col) => {
     const gridCopy = [...grid];
-    const nodeToBeToggled = gridCopy[row][col];
-    const toggledNode = { ...nodeToBeToggled, isWall: !nodeToBeToggled.isWall };
-    gridCopy[row][col] = toggledNode;
+    let nodeToBeToggled = gridCopy[row][col];
 
-    return gridCopy;
-  };
+    const nodeToggled = 
+          toggle === 'wall'
+        ? (nodeToBeToggled = { ...nodeToBeToggled, isWall: !nodeToBeToggled.isWall })
+        : toggle === 'weight'
+        ? (nodeToBeToggled = { ...nodeToBeToggled, isWeight: !nodeToBeToggled.isWeight })
+        : toggle === 'start'
+        ? (nodeToBeToggled = { ...nodeToBeToggled, isStart: !mouseIsPressed })
+        : toggle === 'end'
+        ? (nodeToBeToggled = { ...nodeToBeToggled, isEnd: !mouseIsPressed })
+        : nodeToBeToggled;
 
-  const gridWithToggleWeight = (row, col) => {
-    const gridCopy = [...grid];
-    const nodeToBeToggled = gridCopy[row][col];
-    const toggledNode = { ...nodeToBeToggled, isWeight: !nodeToBeToggled.isWeight };
-
-    console.log(toggledNode);
-    gridCopy[row][col] = toggledNode;
-
-    return gridCopy;
-  };
-
-  const gridWithRelocatedStartEnd = (row, col) => {
-    const gridCopy = [...grid];
-    let nodeToBeRelocated = gridCopy[row][col];
-    if (relocateStart) {
-      if (mouseIsPressed) {
-        nodeToBeRelocated = { ...nodeToBeRelocated, isStart: false };
-      } else {
-        nodeToBeRelocated = { ...nodeToBeRelocated, isStart: true };
-      }
-    }
-    if (relocateEnd) {
-      if (mouseIsPressed) {
-        nodeToBeRelocated = { ...nodeToBeRelocated, isEnd: false };
-      } else {
-        nodeToBeRelocated = { ...nodeToBeRelocated, isEnd: true };
-      }
-    }
-    gridCopy[row][col] = nodeToBeRelocated;
+    gridCopy[row][col] = nodeToggled;
 
     return gridCopy;
   };
