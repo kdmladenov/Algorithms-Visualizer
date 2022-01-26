8fce821f9b0b29cc8d1dae820063cceb67464188
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-01-26 18:03:42 +0200

    Toggle_switch added

--- src/components/pathfinder/Header.js
+++ src/components/pathfinder/Header.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { icons } from '../../constants/constants';
+import './styles/Header.css';
 
-const Header = ({ nodeSize, setNodeSize }) => {
-
-
+const Header = ({ nodeSize, setNodeSize, toggle, setToggle }) => {
   return (
-    <div>
-      <label className="text-warning" htmlFor="nodes-amount">
-        Resize the Grid
-      </label>
-      <input
-        name="nodes-amount"
-        type="range"
-        id="nodes-slider"
-        value={nodeSize}
-        onChange={(e) => setNodeSize(e.target.value)}
-        min="2"
-        max="10"
-      />
+    <div className="header flex">
+      <form className="toggle_switch flex">
+        {['wall', 'weight', 'eraser'].map((item) => (
+          <div key={item}>
+            <input
+              id={item}
+              name="toggle_switch"
+              type="radio"
+              value={item}
+              checked={item === toggle}
+              onChange={(e) => setToggle(e.target.value)}
+            />
+            <label htmlFor={item}>
+              <img src={icons[item]} alt={item} />
+            </label>
+          </div>
+        ))}
+      </form>
+      <>
+        <label className="text-warning" htmlFor="nodes-amount">
+          Resize the Grid
+        </label>
+        <input
+          name="nodes-amount"
+          type="range"
+          id="nodes-slider"
+          value={nodeSize}
+          onChange={(e) => setNodeSize(e.target.value)}
+          min="2"
+          max="10"
+        />
+      </>
     </div>
   );
 };
 
-export default Header
+export default Header;
